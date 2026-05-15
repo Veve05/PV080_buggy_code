9fb4ae576a57878beb3391bceefa3226d71932cb
Veve05 <veverka.skopalova@seznam.cz>
2026-05-15 12:34:15 +0200

    Update buggy_python_code_solution.py

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -5,41 +5,36 @@ import flask
 
 app = flask.Flask(__name__)
 
-
 @app.route("/")
 def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 class Person(object):
     def __init__(self, name):
         self.name = name
 
-
 def print_nametag(format_string, person):
     print(format_string.format(person=person))
-
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
+
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
         print('Exception')
 
-
 def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
@@ -64,4 +59,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
